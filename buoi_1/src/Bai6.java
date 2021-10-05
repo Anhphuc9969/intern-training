c012bd5077def92af6807d9daa3b50baf5ea9835
Anhphuc9969 <you@example.com>
2021-10-06 03:18:10 +0700

    up all

--- buoi_1/src/Bai6.java
+++ buoi_1/src/Bai6.java
@@ -20,11 +20,11 @@ public class Test {
         }
     }
     public static boolean isPrimeNumber(int n) {
-        // so nguyen n < 2 khong phai la so nguyen to
+
         if (n < 2) {
             return false;
         }
-        // check so nguyen to khi n >= 2
+    
         int squareRoot = (int) Math.sqrt(n);
         for (int i = 2; i <= squareRoot; i++) {
             if (n % i == 0) {
